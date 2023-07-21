8c2dbc7f4d5a376480353b7e20faab94c816a13e
Carl Dungca <carl.dungca@yahoo.com>
2023-07-20 23:35:53 -0700

    Prevents app from going to sleep from 9am-9pm

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ var PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`It's alive on http://localhost:${PORT}`));
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.status(200).send('hello world');
+});
+
 app.get('/recommendations/:location', (req, res) => {
   fetch(
     `https://api.yelp.com/v3/businesses/search?location=${req.params.location}&term=bars%2Caquariums%2Cfood%2Cmuseums&categories=&limit=50`,
@@ -37,3 +42,12 @@ app.get('/coordinates/:maxboxLinePathData', (req, res) => {
     .then(res => res.json())
     .then(json => res.status(200).send(json));
 });
+
+// Keeps the app from going to sleep
+setInterval(() => {
+  const hour = new Date().getHours();
+  // Only sends a get request if between 9am and 9pm to save on uptime hours for render.com
+  if (hour >= 9 || hour < 21) {
+    app.get('/');
+  }
+}, 25 * 60 * 1000); // Sends a ping every 25 minutes
